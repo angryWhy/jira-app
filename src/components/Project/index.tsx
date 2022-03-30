e1a2d4dc4f4d240a93923f80f1892b312477a180
angryWhy <875978822@qq.com>
2022-03-30 09:23:36 +0800

    baseCompleted

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -65,7 +65,7 @@ const ProjectList: React.FunctionComponent<IProjectListProps> = (props) => {
     })
     return (<div>
         <SearchPanel param={param} setParam={setParam} users={users}/>
-        <List list={list || []} users={users} loading={loading}/>
+        <List list={list || []} users={users} loading={isLoading}/>
     </div>)
 };
 
